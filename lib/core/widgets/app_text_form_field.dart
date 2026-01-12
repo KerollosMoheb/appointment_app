6f603e6c9c530dd4e4342d362cda651fd208cdce
kerollosmoheb <kokomoheb73@gmail.com>
2026-01-12 13:05:31 +0200

    Refactor login feature and enhance styling
    - Moved LoginScreen to a new file and updated its implementation.
    - Updated import paths for consistency.
    - Added new text styles and color constants.
    - Created reusable AppTextButton and AppTextFormField widgets.
    - Added helper functions for spacing.
    - Introduced FontWeightHelper for better font weight management.
    - Added AlreadyHaveAnAccountText and TermsAndConditionsText widgets for improved UI.

---
+++ lib/core/widgets/app_text_form_field.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_project_omar_ahmed/core/themeing/colors.dart';
+import 'package:flutter_complete_project_omar_ahmed/core/themeing/styles.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextFormField extends StatelessWidget {
+  const AppTextFormField({
+    super.key,
+    required this.hintText,
+    this.suffixIcon,
+    this.isObscureText,
+    this.focusedBorder,
+    this.enabledBorder,
+    this.inputTextStyle,
+    this.hintStyle,
+    this.contentPadding,
+    this.backgroundColor,
+  });
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? focusedBorder;
+  final InputBorder? enabledBorder;
+  final TextStyle? inputTextStyle;
+  final TextStyle? hintStyle;
+  final String hintText;
+  final bool? isObscureText;
+  final Widget? suffixIcon;
+  final Color? backgroundColor;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        contentPadding:
+            contentPadding ??
+            EdgeInsets.symmetric(vertical: 20.w, horizontal: 18.h),
+        focusedBorder:
+            focusedBorder ??
+            OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              borderSide: BorderSide(color: ColorsManager.mainBlue, width: 1.3),
+            ),
+        enabledBorder:
+            enabledBorder ??
+            OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              borderSide: BorderSide(
+                color: ColorsManager.lighterGrey,
+                width: 1.3,
+              ),
+            ),
+        hintStyle: hintStyle ?? TextStyles.font14LightGreyRegular,
+        hintText: hintText,
+        suffixIcon: suffixIcon,
+        fillColor: backgroundColor ?? ColorsManager.morelightGrey,
+        filled: true,
+      ),
+      obscureText: isObscureText ?? false,
+      style: inputTextStyle ?? TextStyles.font14DarkBlueMedium,
+    );
+  }
+}
--- lib/features/onboarding/widgets/get_started_button.dart
+++ lib/features/onboarding/widgets/get_started_button.dart
@@ -19,7 +19,7 @@ class GetStartedButton extends StatelessWidget {
         minimumSize: Size(double.infinity, 52),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       ),
-      child: Text('Get Started', style: TextStyles.font16WhiteSemiBold),
+      child: Text('Get Started', style: TextStyles.font16WhiteMedium),
     );
   }
 }
